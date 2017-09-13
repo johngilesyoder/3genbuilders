661f5c6cb426b47e8b2cebcea44f7ac28801661c
John Yoder <johngilesyoder@gmail.com>
2017-09-13 12:19:08 -0500

    Copy update

--- wp-content/themes/3gen/includes/about.php
+++ wp-content/themes/3gen/includes/about.php
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-md-8 col-md-offset-2">
         <h3>About 3Gen Builders</h3>
-        <p>3Gen Builders have been Missoula's affordable homebuilders for over 70 years. We specialize in new home construction. Our primary area of focus is affordable homes, with the majority of our houses priced less than $325,000.</p>
+        <p>3Gen Builders have been Missoula's affordable homebuilders for over 60 years. We specialize in new home construction. Our primary area of focus is affordable homes, with the majority of our houses priced less than $325,000.</p>
         <p>We use thoughtful floor plans in order to maximize the living space of each home while delivering the greatest value possible to our homeowners.</p>
         <p>Our website showcases our current projects but we work with local developers on an ongoing basis to secure building lots for future projects. We select and secure lots that enable us to deliver affordable housing to the Missoula and surrounding markets.</p>
         <p>Please <a href="mailto:paul@3genbuilders.com">contact us</a> to discuss how we can work together to build your new home.</p>
